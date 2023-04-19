f1681d98c677d11a8b5ea199dc2004c668c6f590
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-19 20:41:15 +0500

    Update PriorityQueueCreator.java

--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,28 @@
 package com.epam.collections.queue;
 
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>(new MyComparator());
+            priorityQueue.addAll(firstList);
+            priorityQueue.addAll(secondList);
+
+        return priorityQueue;
+
+    }
+
+    class MyComparator implements Comparator<String> {
+
+        @Override
+        public int compare(String o1, String o2) {
+            int value = o1.compareTo(o2);
+            if (value > 0) {
+                return -1;
+            } else if (value < 0) {
+                return 1;
+            }
+            return 0;
+        
     }
 }
